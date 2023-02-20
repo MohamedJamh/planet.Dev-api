aed9f913912c5828c4febd1db7358022926976c1
Mehdi Qaos <mehdi.qaos@gmail.com>
2023-02-20 17:24:30 +0100

    ajout des migration et models et les relations entre models

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
