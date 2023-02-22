28b8e43db5a0c98f9eac192d7a3459c895ae1f2b
issam <issammez44@gmail.com>
2023-02-22 20:16:27 +0100

    add store method so you can store the articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Http\Resources\ArticleResource;
 use App\Http\Resources\ArticleCollection;
+use App\Http\Requests\StoreArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -17,5 +18,10 @@ class ArticleController extends Controller
     public function show(Article $article)
     {
         return new ArticleResource($article);
+    }
+
+    public function store(StoreArticleRequest $request)
+    {
+        return new ArticleResource(Article::create($request->all()));
     }
 }
