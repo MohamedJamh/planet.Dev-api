ba7197755e27306af75de619229c4d6b122f46d4
Barnoch Anas <anasbarnoch@gmail.com>
2023-02-23 11:16:14 +0100

    PDA-2 JWT Done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TestController;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
 });
+
+Route::controller(TestController::class)->middleware('auth:api')->group(function () {
+    Route::get('home', 'index');
+});
+
