03fb7aca9a57d1aef97ccc6ba36acf84e424ce89
Mehdi Qaos <mehdi.qaos@gmail.com>
2023-02-23 14:14:52 +0100

    added auth middleware to the constructor of CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,10 +10,10 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     public function index()
     {
