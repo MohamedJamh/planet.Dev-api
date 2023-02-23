9188c3931becaa6945d1d6b525d005eea276b8ab
issam <issammez44@gmail.com>
2023-02-23 16:34:43 +0100

    show the tags in tha articles

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -14,13 +14,21 @@ class ArticleResource extends JsonResource
      */
     public function toArray($request)
     {
+        $tags = [];
+        foreach($this->tags as $tag) {
+            $tags[] = [
+                'id' => $tag->id,
+                'name' => $tag->name
+            ];
+        }
         return [
             'id' => $this->id,
             'title' => $this->title,
             'body' => $this->body,
             'userId' => $this->user_id,
             'categoryId' => $this->category_id,
-            'category' =>$this->category
+            'category' => $this->category->name,
+            'tags' => $tags
         ];
     }
 }
