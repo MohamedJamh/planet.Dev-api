b0eb0855ab4b024c05ee439c047882be74eedfc1
MohamedJamh <jam7.mohamed@gamil.com>
2023-02-23 11:49:30 +0100

    auth controller middlwear

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -11,6 +11,9 @@ use App\Http\Resources\CommentCollection;
 
 class CommentsController extends Controller
 {
+    public function __construct(){
+        $this->middleware(['auth:api']);
+    }
     /**
      * Display a listing of the resource.
      *
