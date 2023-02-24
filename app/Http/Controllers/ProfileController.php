c0ce809c5e019026ba477c99d6546d7ff253ecd2
MohamedJamh <jam7.mohamed@gmail.com>
2023-02-24 08:30:55 +0100

    PDA-11 Setup Profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     //
+    public function index(){
+        //retriving user profile info
+    }
+
+    public function updateName(){
+        //update user first name or last name
+    }
+    public function updateEmail(){
+        //update user eamil | request password
+    }
+    public function updatePassword(){
+        //update user password | request password
+    }
 }
